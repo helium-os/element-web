c5be411543d6ff32da209c2f8bfe3c5da3c88be7
丁一娉 <dingyp@easypayx.com>
2023-09-19 19:44:51 +0800

    heliumos#455 eslint test7

--- src/vector/app.tsx
+++ src/vector/app.tsx
@@ -99,13 +99,14 @@ function getOrgId(): string {
 }
 
 export async function loadApp(fragParams: {}): Promise<ReactElement> {
-    console.log('test4', {
+    console.log('test5', {
         a: 1,
         b: 2,
         c: 3,
         d: 4,
         e: 5,
-        f:6
+        f:6,
+        g:7
     })
     initRouting();
     const platform = PlatformPeg.get();
