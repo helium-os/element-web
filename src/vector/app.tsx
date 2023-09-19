cdb454dfcde6347645914103d8b41e7db1a385d8
丁一娉 <dingyp@easypayx.com>
2023-09-19 20:49:02 +0800

    heliumos#455 delete test code

--- src/vector/app.tsx
+++ src/vector/app.tsx
@@ -101,19 +101,6 @@ function getOrgId(): string {
 }
 
 export async function loadApp(fragParams): Promise<ReactElement> {
-    console.log("test7", {
-        a: 1,
-        b: 2,
-        c: 3,
-        d: 4,
-        e: 5,
-        f: 6,
-        g: 7,
-        h: 8,
-        i: 9,
-        j: 10,
-        k: 11,
-    });
     initRouting();
     const platform = PlatformPeg.get();
 
