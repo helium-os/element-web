a68e11ca505cfaf1aad30bf5d2fa8d4f8e369651
丁一娉 <dingyp@easypayx.com>
2023-09-21 16:36:33 +0800

    heliumos#455 eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,50 +23,23 @@ module.exports = {
         "plugin:prettier/recommended", // eslint-plugin-prettier
     ],
     rules: {
-        // // 取消函数参数需要重新赋值给另一个变量才能使用
-        // 'no-param-reassign': ['off'],
-        // // 取消 { a, b, c } 多个变量需要换行
-        // 'object-curly-newline': ['off'],
-        // // 禁用var，用let和const代替
-        // 'no-var': 'error',
-        // // 强制全等( === 和 !==)
-        // eqeqeq: 'error',
-        // // 函数名称或function关键字与左括号之间不允许有空格
-        // 'space-before-function-paren': ['error', 'never'],
-        // // 箭头函数，箭头前后空格
-        // 'arrow-spacing': ['error', { before: true, after: true }],
-        // 'padding-line-between-statements': ['error', {
-        //     blankLine: 'always', prev: '*', next: 'return'  // return 之前加空行
-        // }],
-        //
-        // // react配置
-        // // 非空标签和自关闭标签闭合标记必须与开始标记对齐
-        // 'react/jsx-closing-bracket-location': ['error', {
-        //     "nonEmpty": 'line-aligned',
-        //     "selfClosing": 'line-aligned'
-        // }],
+        // 函数括号前的空格
+        "space-before-function-paren": [
+            "error",
+            {
+                anonymous: "never", // 匿名函数不需要空格
+                named: "never", // 命名函数不需要空格
+                asyncArrow: "always", // 箭头函数需要空格
+            },
+        ],
+        // eslint-plugin-react
         // 大括号内的换行符
-        // "react/jsx-curly-newline": ["error", { multiline: "require", singleline: "require" }],
-        // // 当多行时，每一行的最多props个数为1
-        // "react/jsx-max-props-per-line": ['error', { when: "multiline", maximum: 1 }],
+        "react/jsx-curly-newline": ["error", { multiline: "require", singleline: "consistent" }],
+        // 大括号内的空格
+        "react/jsx-curly-spacing": ["error", { when: "never", children: { when: "always" } }],
         // 禁止target="_blank"不带rel="noreferrer"属性
         "react/jsx-no-target-blank": ["off"],
-        // "react/jsx-wrap-multilines": ['error', {
-        //     "declaration": "parens-new-line",
-        //     "assignment": "parens-new-line",
-        //     "return": "parens-new-line",
-        //     "arrow": "parens-new-line",
-        //     "condition": "parens-new-line",
-        //     "logical": "parens-new-line",
-        //     "prop": "parens-new-line"
-        // }],
-        // // 强制组件方法顺序
-        // 'react/sort-comp': ['off'],
-        // // 结束标签，组件省略闭合标签，html不省略闭合标签
-        // 'react/self-closing-comp': ['error', { component: true, html: false }],
-        //
-        // // 禁止出现未使用的变量
-        // '@typescript-eslint/no-unused-vars': ['off'],
+        // eslint-plugin-import
         "import/no-unresolved": ["off"],
     },
 };
