59478a03097a0fa37132de36212266edb9a1a3ac
丁一娉 <dingyp@easypayx.com>
2023-09-22 17:14:05 +0800

    heliumos#466 add log

--- src/vector/rewrite-js-sdk/mediaHandler.ts
+++ src/vector/rewrite-js-sdk/mediaHandler.ts
@@ -5,7 +5,8 @@ const _getUserMediaStream = MediaHandler.prototype.getUserMediaStream;
 // @ts-ignore
 MediaHandler.prototype.getUserMediaStream = async function (...args): Promise<MediaStream | string> {
     try {
-        await askForMediaAccess();
+        const res = await askForMediaAccess();
+        console.log("获取到结果", res);
         return _getUserMediaStream.call(this, ...args);
     } catch (error) {
         console.log("desktop没有打开录音&摄像头权限，error is", error);
