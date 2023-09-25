cc1b4a4e361b31e7ff4368842edb70cb4bd3604c
丁一娉 <dingyp@easypayx.com>
2023-09-25 17:18:39 +0800

    heliumos#466 eslint修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
             jsx: true,
         },
     },
-    plugins: ["@typescript-eslint", "import", "react", "react-hooks"],
+    plugins: ["@typescript-eslint"],
     extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
@@ -23,19 +23,23 @@ module.exports = {
         "plugin:prettier/recommended", // eslint-config-prettier
     ],
     rules: {
-        // 关闭禁止使用未声明的变量
+        // 关闭未使用的变量报错
         "no-unused-vars": "off",
         // 允许在对象上直接调用原型上的方法
         "no-prototype-builtins": "off",
         // 不允许空块语句（catch代码块除外）
         "no-empty": ["error", { allowEmptyCatch: true }],
 
-        // ------------@typescript-eslint
+        // ------------@typescript-eslint/eslint-plugin
+        // 关闭必须使用 ES6 样式导入或import foo = require("foo")导入
         "@typescript-eslint/no-var-requires": "off",
+        // 关闭未使用的变量报错
         "@typescript-eslint/no-unused-vars": "off",
+        // 允许所有@ts-<directive>的使用
         "@typescript-eslint/ban-ts-comment": "off",
+        // 允许使用any
         "@typescript-eslint/no-explicit-any": "off",
-        // 关闭禁用某些类型
+        // 关闭禁用某些类型（Boolean/String/Number等）
         "@typescript-eslint/ban-types": "off",
 
         // ------------eslint-plugin-react
@@ -47,6 +51,7 @@ module.exports = {
         "react/no-render-return-value": "off",
 
         // ------------eslint-plugin-import
+        // 允许导入未解析到的模块
         "import/no-unresolved": ["off"],
     },
 };
