3cba931d9f704678c9b2a3b10e0bcb1dd6c3efa9
丁一娉 <dingyp@easypayx.com>
2023-09-27 12:52:50 +0800

    heliumos#480 eslint修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
             jsx: true,
         },
     },
-    plugins: ["@typescript-eslint", "import", "react", "react-hooks"],
+    plugins: ["@typescript-eslint"],
     extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
@@ -23,23 +23,37 @@ module.exports = {
         "plugin:prettier/recommended", // eslint-config-prettier
     ],
     rules: {
-        // 函数括号前的空格
-        "space-before-function-paren": [
-            "error",
-            {
-                anonymous: "never", // 匿名函数不需要空格
-                named: "never", // 命名函数不需要空格
-                asyncArrow: "always", // 箭头函数需要空格
-            },
-        ],
-        // eslint-plugin-react
-        // 大括号内的换行符
-        // "react/jsx-curly-newline": ["error", { multiline: "require", singleline: "consistent" }],
-        // 大括号内的空格
-        "react/jsx-curly-spacing": ["error", { when: "never", children: { when: "always" } }],
-        // 禁止target="_blank"不带rel="noreferrer"属性
-        "react/jsx-no-target-blank": ["off"],
-        // eslint-plugin-import
-        "import/no-unresolved": ["off"],
+        // 提醒有未使用的变量
+        "no-unused-vars": "warn",
+        // 允许在对象上直接调用原型上的方法
+        "no-prototype-builtins": "off",
+        // 不允许空块语句（catch代码块除外）
+        "no-empty": ["error", { allowEmptyCatch: true }],
+
+        // ------------@typescript-eslint/eslint-plugin
+        // 关闭必须使用 ES6 样式导入或import foo = require("foo")导入
+        "@typescript-eslint/no-var-requires": "off",
+        // 提醒有未使用的变量
+        "@typescript-eslint/no-unused-vars": "warn",
+        // 允许所有@ts-<directive>的使用
+        "@typescript-eslint/ban-ts-comment": "off",
+        // 允许使用any
+        "@typescript-eslint/no-explicit-any": "off",
+        // 提醒禁用某些类型（Boolean/String/Number等）
+        "@typescript-eslint/ban-types": "warn",
+
+        // ------------eslint-plugin-react
+        // 关闭target="_blank"不带rel="noreferrer"属性校验
+        "react/jsx-no-target-blank": "off",
+        // 提醒使用了已弃用的方法（当前项目需要）
+        "react/no-deprecated": "warn",
+        // 提醒使用了ReactDom.render()的返回值（当前项目需要）
+        "react/no-render-return-value": "warn",
+        // 提醒使用了findDOMNode方法（当前项目需要）
+        "react/no-find-dom-node": "warn",
+
+        // ------------eslint-plugin-import
+        // 提醒导入了未解析到的模块
+        "import/no-unresolved": ["warn"],
     },
 };
