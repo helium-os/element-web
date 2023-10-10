951c867353bbf00a1a186fd3f9e1890e0f91e744
丁一娉 <dingyp@easypayx.com>
2023-10-10 18:11:09 +0800

    heliumos#516 判断管理员是否离开方法修改

--- src/vector/rewrite-js-sdk/room-state.ts
+++ src/vector/rewrite-js-sdk/room-state.ts
@@ -1,15 +1,23 @@
-import {RoomState} from "matrix-js-sdk/src/models/room-state";
+import { RoomState } from "matrix-js-sdk/src/models/room-state";
+import { isPeopleRoom } from "./room";
 
 /**
  * 判断管理员是否已离开房间
  *
  * 用来实现以下交互：
  * 私聊里，一方主动离开之后，另外一方除了看聊天记录以外不能做其他操作；
- * 群聊里，管理员离开后， 其他人除了看聊天记录以外不能做其他操作；
- * tips: 私聊里因为两人都为管理员角色，所以可以用此条件来判断是否有一方离开了
+ * 群聊里，管理员离开后，其他人除了看聊天记录以外不能做其他操作；
+ *
+ *
+ * tips:
+ * 私聊里，因为两人都为管理员角色，所以有一人离开后，即视为管理员离开
+ * 群聊里，可能会有多个管理员（比如社区管理员可以赋予其他人管理员角色），所以当所有管理员都离开时，才视为管理员离开
  */
-RoomState.prototype.isAdminLeft = function(): boolean {
+RoomState.prototype.isAdminLeft = function (): boolean {
     const members = this.getMembers();
-    const adminMembers = members.filter(item => item.isAdmin());
-    return !!adminMembers.find(item => item.membership === 'leave');
-}
+    const adminMembers = members.filter((item) => item.isAdmin());
+    if (!adminMembers.length) return false; // 创建房间阶段，还未分配管理员
+    return isPeopleRoom(this.roomId)
+        ? adminMembers.some((item) => item.membership === "leave")
+        : !adminMembers.some((item) => item.membership === "join");
+};
