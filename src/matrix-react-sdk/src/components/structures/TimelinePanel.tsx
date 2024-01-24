132861bd7ecc322c903046b3526f5c45c63ba940
丁一娉 <dingyp@easypayx.com>
2024-01-24 11:38:55 +0800

    heliumos#757 read marker add log

--- src/matrix-react-sdk/src/components/structures/TimelinePanel.tsx
+++ src/matrix-react-sdk/src/components/structures/TimelinePanel.tsx
@@ -989,10 +989,33 @@ class TimelinePanel extends React.Component<IProps, IState> {
         }
     }
 
+    private getRatio = () => {
+        let ratio = 0;
+        const screen = window.screen;
+        const ua = navigator.userAgent.toLowerCase();
+
+        if (window.devicePixelRatio !== undefined) {
+            ratio = window.devicePixelRatio;
+        } else if (~ua.indexOf("msie")) {
+            if (screen.deviceXDPI && screen.logicalXDPI) {
+                ratio = screen.deviceXDPI / screen.logicalXDPI;
+            }
+        } else if (window.outerWidth !== undefined && window.innerWidth !== undefined) {
+            ratio = window.outerWidth / window.innerWidth;
+        }
+
+        if (ratio) {
+            ratio = Math.round(ratio * 100);
+        }
+        return ratio;
+    };
+
     private sendReadReceipt = (): void => {
         console.log(
             "------------------------------------readMarker bugfix enter sendReadReceipt-------------------------------------",
         );
+
+        console.log("readMarker bugfix getRatio", this.getRatio());
         if (SettingsStore.getValue("lowBandwidth")) return;
 
         if (!this.messagePanel.current) return;
