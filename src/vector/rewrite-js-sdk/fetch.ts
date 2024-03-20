8a32a055f8326ea5d8b060a6eacc09a5296d658d
丁一娉 <dingyp@easypayx.com>
2024-03-20 12:44:37 +0800

    heliumos#838 fetch请求允许携带cookie

--- src/vector/rewrite-js-sdk/fetch.ts
+++ src/vector/rewrite-js-sdk/fetch.ts
@@ -2,8 +2,10 @@ import { FetchHttpApi } from "matrix-js-sdk/src/http-api/fetch";
 import { isInDesktop } from "matrix-react-sdk/src/utils/env";
 
 const _fetch = FetchHttpApi.prototype.fetch;
-FetchHttpApi.prototype.fetch = function (...[resource, ...rest]) {
-    console.log("isInDesktop", isInDesktop);
+FetchHttpApi.prototype.fetch = function (resource, options) {
     const finalResource = !isInDesktop && resource instanceof URL ? resource.pathname : resource; // 网页端做请求拦截
-    return _fetch.call(this, ...[finalResource, ...rest]);
+    return _fetch.call(this, finalResource, {
+        ...options,
+        ...(isInDesktop ? {} : { credentials: "include" }), // 网页端需要携带cookie
+    });
 };
