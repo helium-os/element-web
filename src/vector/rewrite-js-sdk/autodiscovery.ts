8a9c2b5d259dc88c2740946a7364fb87a26702b1
丁一娉 <dingyp@easypayx.com>
2024-03-20 17:54:24 +0800

    heliumos#838 debug

--- src/vector/rewrite-js-sdk/autodiscovery.ts
+++ src/vector/rewrite-js-sdk/autodiscovery.ts
@@ -3,8 +3,15 @@ import { needRequestIntercept } from "./fetch";
 
 const _fetch = AutoDiscovery.fetch;
 AutoDiscovery.fetch = function (resource, options) {
-    const finalResource = needRequestIntercept && resource instanceof URL ? resource.pathname : resource; // 网页端做请求拦截
-    console.log("AutoDiscovery finalResource", finalResource, "needRequestIntercept", needRequestIntercept);
+    let finalResource = resource;
+    // 网页端做请求拦截
+    if (needRequestIntercept) {
+        finalResource =
+            resource instanceof URL
+                ? resource.pathname
+                : (finalResource as string).replace("https://matrix.system.service.heliumos", "");
+    }
+    console.log("AutoDiscovery finalResource", finalResource, "resource", resource);
     return _fetch.call(this, finalResource, {
         ...options,
         ...(needRequestIntercept ? { credentials: "include" } : {}), // 网页端需要携带cookie
