cd1fda2edfd83cc6ec56dade41e42bf654b89938
丁一娉 <dingyp@easypayx.com>
2024-03-20 13:17:08 +0800

    heliumos#838 debug

--- src/vector/rewrite-js-sdk/fetch.ts
+++ src/vector/rewrite-js-sdk/fetch.ts
@@ -6,6 +6,6 @@ FetchHttpApi.prototype.fetch = function (resource, options) {
     const finalResource = !isInDesktop && resource instanceof URL ? resource.pathname : resource; // 网页端做请求拦截
     return _fetch.call(this, finalResource, {
         ...options,
-        ...(isInDesktop ? {} : { credentials: "include" }), // 网页端需要携带cookie
+        // ...(isInDesktop ? {} : { credentials: "include" }), // 网页端需要携带cookie
     });
 };
