837c6983140c019ddbe8ce4020c0482b05c11286
丁一娉 <dingyp@easypayx.com>
2024-03-20 17:43:30 +0800

    heliumos#838 debug

--- src/vector/rewrite-js-sdk/autodiscovery.ts
+++ src/vector/rewrite-js-sdk/autodiscovery.ts
@@ -4,6 +4,7 @@ import { needRequestIntercept } from "./fetch";
 const _fetch = AutoDiscovery.fetch;
 AutoDiscovery.fetch = function (resource, options) {
     const finalResource = needRequestIntercept && resource instanceof URL ? resource.pathname : resource; // 网页端做请求拦截
+    console.log("AutoDiscovery finalResource", finalResource, "needRequestIntercept", needRequestIntercept);
     return _fetch.call(this, finalResource, {
         ...options,
         ...(needRequestIntercept ? { credentials: "include" } : {}), // 网页端需要携带cookie
