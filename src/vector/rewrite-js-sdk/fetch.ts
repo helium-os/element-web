4dab115955a84c018d24fac19e687aea94d591c8
丁一娉 <dingyp@intellimesh.io>
2024-03-29 11:01:30 +0800

    hos#838 代码优化

--- src/vector/rewrite-js-sdk/fetch.ts
+++ src/vector/rewrite-js-sdk/fetch.ts
@@ -25,16 +25,14 @@ export function getRequestOptions(options) {
 
 // 拦截fetch请求
 const _fetch = window.fetch;
-window.fetch = async function (resource, options) {
+window.fetch = function (resource, options) {
     if (resource instanceof Request) {
-        const response = await _fetch({
+        return _fetch({
             ...resource,
             url: getRequestResource(resource.url) as string,
             credentials: getCredentials(),
         });
-        return response;
     }
 
-    const response = await _fetch(getRequestResource(resource), getRequestOptions(options));
-    return response;
+    return _fetch(getRequestResource(resource), getRequestOptions(options));
 };
