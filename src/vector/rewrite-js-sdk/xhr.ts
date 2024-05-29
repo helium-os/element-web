fb52b976ca70c36911d551f5af9885c9ed8d8b0f
丁一娉 <dingyp@intellimesh.io>
2024-05-29 19:11:25 +0800

    hos#70 解决ts报错

--- src/vector/rewrite-js-sdk/xhr.ts
+++ src/vector/rewrite-js-sdk/xhr.ts
@@ -3,5 +3,5 @@ import { getRequestUrl } from "./fetch";
 // 拦截xhr open方法，请求时对url做处理
 const _open = XMLHttpRequest.prototype.open;
 XMLHttpRequest.prototype.open = function (method, url, ...rest) {
-    return _open.call(this, method, getRequestUrl(url), ...rest);
+    return _open.apply(this, [method, getRequestUrl(url), ...rest]);
 };
