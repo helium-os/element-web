e78929a53b39bae1528ba85b99680d2940deb7ff
丁一娉 <dingyp@intellimesh.io>
2024-06-12 19:04:50 +0800

    hos#70 添加注释

--- src/matrix-react-sdk/src/components/structures/MatrixChat.tsx
+++ src/matrix-react-sdk/src/components/structures/MatrixChat.tsx
@@ -478,7 +478,7 @@ export default class MatrixChat extends React.PureComponent<IProps, IState> {
         window.addEventListener(
             "message",
             (event) => {
-                console.log("Chat window 订阅到message", event);
+                console.log("Chat window 订阅到message", event.data);
                 this.onReceiveMessage(event);
             },
             false,
@@ -487,7 +487,7 @@ export default class MatrixChat extends React.PureComponent<IProps, IState> {
         document.addEventListener(
             "message",
             (event) => {
-                console.log("Chat document 订阅到message", event);
+                console.log("Chat document 订阅到message", event.data);
                 this.onReceiveMessage(event);
             },
             false,
